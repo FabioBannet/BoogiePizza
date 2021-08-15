260660300c40a28735d61284e3045d668bc6f131
fabioBannet <dimapruglo@gmail.com>
2021-08-15 20:01:25 +0300

    1.4. Customizing SpringApplication If the SpringApplication defaults are not to your taste, you can instead create a local instance and customize it. For example, to turn off the banner, you could write:
    from: https://docs.spring.io/spring-boot/docs/current/reference/html/features.html#features.external-config

--- src/main/java/com/boogiepizza/BoogiePizza/BoogiePizzaApplication.java
+++ src/main/java/com/boogiepizza/BoogiePizza/BoogiePizzaApplication.java
@@ -1,5 +1,6 @@
 package com.boogiepizza.BoogiePizza;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BoogiePizzaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BoogiePizzaApplication.class, args);
+		SpringApplication application = new SpringApplication(BoogiePizzaApplication.class);
+		application.setBannerMode(Banner.Mode.OFF);
+		application.run(args);
 	}
 }
