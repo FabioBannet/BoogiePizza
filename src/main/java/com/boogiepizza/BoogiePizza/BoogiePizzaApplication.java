856c1efe56d2ac6f227db391bbc83eb086db731c
fabioBannet <dimapruglo@gmail.com>
2021-08-16 16:56:45 +0300

    initial commit

--- src/main/java/com/boogiepizza/BoogiePizza/BoogiePizzaApplication.java
+++ src/main/java/com/boogiepizza/BoogiePizza/BoogiePizzaApplication.java
@@ -1,6 +1,5 @@
 package com.boogiepizza.BoogiePizza;
 
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BoogiePizzaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication application = new SpringApplication(BoogiePizzaApplication.class);
-		application.setBannerMode(Banner.Mode.OFF);
-		application.run(args);
+		SpringApplication.run(BoogiePizzaApplication.class, args);
 	}
 }
